4e43a85e5c867caae268bd17116a0b5b1a6387d9
Bohdan <105508224+den-ffv@users.noreply.github.com>
2022-12-13 14:48:33 +0200

    ready project, table with js

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const UL = document.querySelectorAll('ul>li>span');
+
+document.querySelector('table').onclick = function(event) {
+  
+  if(event.target.tagName !== 'TD') return false;
+ let data = [...event.target.parentNode.children];
+ let text = getDataFromTD(data);
+ outText(text);
+};
+function getDataFromTD(tdArr) {
+  // return tdArr.map(item => item.textContent);
+  let arr = [];
+  for(let i = 0; i < tdArr.length; i++){
+    arr.push(tdArr[i].textContent);
+  }
+  return arr;
+}
+function outText(text) {
+  // text.forEach((item, index) => UL[index].textContent = item);
+  for(let i = 0; i < text.length; i++){
+    UL[i].textContent = text[i];
+  }
+
+}
